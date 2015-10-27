98c0e24676564e264a7620ab49c13afd17dcea4f
mosaick <mosaick.cn@navercorp.com>
2015-10-27 14:30:24 +0900

    init

---
+++ ps/src/main/java/seattle/algostar/ps/acmicpc/temp/Problem1000.java
@@ -0,0 +1,10 @@
+package seattle.algostar.ps.acmicpc.temp;
+
+import java.util.Scanner;
+
+public class Problem1000 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt() + sc.nextInt());
+    }
+}
