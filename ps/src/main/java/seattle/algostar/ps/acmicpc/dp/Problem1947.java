c1f5329c94c2b4dd0275d147d8f00ee1ee2de9be
nbp <yonggyun.jung@navercorp.com>
2015-10-29 17:07:45 +0900

    1947번 commit

---
+++ ps/src/main/java/seattle/algostar/ps/acmicpc/dp/Problem1947.java
@@ -0,0 +1,24 @@
+package com.naver.shopping.codingPractice.dp;
+
+import java.util.Scanner;
+
+public class Problem1947 {
+	int k;
+	long[] cache;
+	
+	public static void main(String[] args){
+		new Problem1947().run();
+	}
+	
+	public void run(){
+		Scanner sc = new Scanner(System.in);
+		k = sc.nextInt(); cache = new long[2];
+		
+		cache[0] = 1;
+		cache[1] = 0;
+		for(int i = 2; i <= k; i++){
+			cache[i % 2] = ((i - 1) * ( cache[(i - 1) % 2] + cache[(i - 2) % 2])) % 1000000000L;
+		}
+		System.out.println(cache[k % 2]);
+	}
+}
