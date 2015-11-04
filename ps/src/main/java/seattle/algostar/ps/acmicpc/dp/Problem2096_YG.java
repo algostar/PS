a6759803b3ceb33ba5e9b370608c4a63cc1a8787
nbp <yonggyun.jung@navercorp.com>
2015-11-04 17:44:12 +0900

    2096, 2216 commit

---
+++ ps/src/main/java/seattle/algostar/ps/acmicpc/dp/Problem2096_YG.java
@@ -0,0 +1,51 @@
+/*
+ * @(#)Problem2096.java $version 2015. 10. 30.
+ *
+ * Copyright 2014 NHN Corp. All rights Reserved. 
+ * NHN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+package com.naver.shopping.codingPractice.dp;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Problem2096_YG {
+	public static void main(String[] args){
+		new Problem2096_YG().run();
+	}
+	
+	int n;
+	int[][] a;
+	int[][] max;
+	int[][] min;
+	
+	public void run(){
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		
+		max = new int[n+2][3]; min = new int[n+2][3]; a = new int[n+2][3];
+		
+		for(int i = 1; i <= n; i++){
+			Arrays.fill(min[i], Integer.MAX_VALUE);
+		}
+		
+		for(int i = 1; i<=n; i++){
+			for(int j = 0; j < 3; j++){
+				a[i][j] = sc.nextInt();
+			}
+		}
+		
+		for(int i = 1; i<=n+1; i++){
+			max[i][0] = Math.max(max[i-1][0], max[i-1][1]) + a[i][0];
+			max[i][1] = Math.max(Math.max(max[i-1][0], max[i-1][1]), max[i-1][2]) + a[i][1];
+			max[i][2] = Math.max(max[i-1][1], max[i-1][2]) + a[i][2];
+			
+			min[i][0] = Math.min(min[i-1][0], min[i-1][1]) + a[i][0];
+			min[i][1] = Math.min(Math.min(min[i-1][0], min[i-1][1]), min[i-1][2]) + a[i][1];
+			min[i][2] = Math.min(min[i-1][1], min[i-1][2]) + a[i][2];
+		}
+		
+		System.out.println(max[n+1][1] + " " + min[n+1][1]);
+	}
+}
---
+++ ps/src/main/java/seattle/algostar/ps/acmicpc/dp/Problem2216_YG.java
@@ -0,0 +1,59 @@
+/*
+ * @(#)Problem2216.java $version 2015. 11. 4.
+ *
+ * Copyright 2014 NHN Corp. All rights Reserved. 
+ * NHN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+package com.naver.shopping.codingPractice.dp;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Problem2216_YG {
+	public static void main(String[] args){
+		new Problem2216_YG().run();
+	}
+	
+	int a,b,c;
+	public void run(){
+		Scanner sc = new Scanner(System.in);
+		a = sc.nextInt(); b = sc.nextInt(); c = sc.nextInt();
+		
+		String input1 = sc.next();
+		String input2 = sc.next();
+		
+		int input1L = input1.length();
+		int input2L = input2.length();
+		int maxLength = Math.max(input1.length(), input2.length());
+		
+		int[][] cache = new int[maxLength + 1][maxLength + 1];
+				
+		for(int i = 0; i < maxLength + 1; i++){
+			Arrays.fill(cache[i], Integer.MIN_VALUE);
+		}
+		
+		for(int i = 0; i <= input1L; i++){
+			cache[i][0] = i * b;
+		}
+		
+		for(int i = 0; i <= input2L; i++){
+			cache[0][i] = i * b;
+		}
+		
+		for(int i = 1; i <= input1L; i++){
+			for(int j = 1; j <= input2L; j++){
+				if(input1.charAt(i - 1) == input2.charAt(j - 1)){
+					cache[i][j] = Math.max(cache[i][j], cache[i -1][j - 1] + a);
+				}else{
+					cache[i][j] = Math.max(cache[i][j], cache[i -1][j - 1] + c);
+				}
+				
+				cache[i][j] = Math.max(cache[i][j], cache[i][j - 1] + b);
+				cache[i][j] = Math.max(cache[i][j], cache[i - 1][j] + b);
+			}
+		}
+		
+		System.out.println(cache[input1L][input2L]);
+	}
+}
