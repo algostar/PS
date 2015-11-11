df412d83e22c60d30167cf43406b5a34c1b7a0a7
jiwan, Jeong <mosaick2@naver.com>
2015-11-12 04:28:26 +0900

    1149

--- ps/src/main/java/seattle/algostar/ps/acmicpc/temp/Problem1000.java
+++ ps/src/main/java/seattle/algostar/ps/acmicpc/temp/Problem1000.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class Problem1000 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println(sc.nextInt() + sc.nextInt());
+        for (int i = 0; i < 1000; i++) {
+        	System.out.println("0 0 0");
+        }
     }
 }
