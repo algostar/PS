cad5041b12cf311494c00974ff7243152a832c25
jiwan, Jeong <mosaick2@naver.com>
2015-11-23 22:38:17 +0900

    10990, 1365, 2258

---
+++ ps/src/main/java/seattle/algostar/ps/acmicpc/dp/Problem2258_JW.java
@@ -0,0 +1,98 @@
+package seattle.algostar.ps.acmicpc.dp;
+
+import java.io.*;
+import java.util.*;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+public class Problem2258_JW {
+	public static void main(String[] args) throws IOException {
+		new Problem2258_JW().run();
+	}
+
+	int N, M;
+	Meat[] meats;
+	int[] wsum;
+	int[] psum;
+	private void run() throws IOException {
+		R.init(System.in);
+		N = R.nextInt();
+		M = R.nextInt();
+		meats = new Meat[N];
+		for (int i = 0; i < N; i++) {
+			meats[i] = new Meat(R.nextInt(), R.nextInt());
+		}
+		Arrays.sort(meats);
+		wsum = new int[N];
+		psum = new int[N];
+		wsum[0] = meats[0].weight;
+		psum[0] = meats[0].price;
+		for (int i = 1; i < N; i++) {
+			wsum[i] = wsum[i-1] + meats[i].weight;
+			if (meats[i-1].weight == meats[i].weight) {
+				psum[i] = meats[i].price + psum[i-1];
+			} else {
+				psum[i] = meats[i].price;
+			}
+		}
+		
+		int minPrice = Integer.MAX_VALUE;
+		for (int i = 0; i < N; i++) {
+			if (wsum[i] >= M) {
+				minPrice = Math.min(minPrice, psum[i]);
+			}
+		}
+		
+		if (minPrice == Integer.MAX_VALUE) minPrice = -1;
+		System.out.println(minPrice);
+	}
+	
+
+	class Meat implements Comparable<Meat> {
+		int weight;
+		int price;
+		public Meat(int w, int p) {
+			this.weight = w;
+			this.price = p;
+		}
+		public int compareTo(Meat o) {
+			int r = this.weight - o.weight;
+			if (r == 0) {
+				r = this.price - o.price;
+			}
+			return r;
+		}
+		@Override
+		public String toString() {
+			return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+		}
+	}
+	static class R {  
+	    static BufferedReader reader;
+	    static StringTokenizer tokenizer;
+
+	    static void init(InputStream input) {
+	        reader = new BufferedReader(
+	                     new InputStreamReader(input) );
+	        tokenizer = new StringTokenizer("");
+	    }
+
+	    static String next() throws IOException {
+	        while ( ! tokenizer.hasMoreTokens() ) {
+	            tokenizer = new StringTokenizer(
+	                   reader.readLine() );
+	        }
+	        return tokenizer.nextToken();
+	    }
+
+	    static int nextInt() throws IOException {
+	        return Integer.parseInt( next() );
+	    }
+		
+	    static double nextDouble() throws IOException {
+	        return Double.parseDouble( next() );
+	    }
+	}
+}
--- ps/src/main/java/seattle/algostar/ps/acmicpc/library/code/SystemInputReader.java
+++ ps/src/main/java/seattle/algostar/ps/acmicpc/library/code/SystemInputReader.java
@@ -12,38 +12,37 @@ public class SystemInputReader {
 		Reader.init(System.in);
 		int n = Reader.nextInt();
 	}
-}
-
-/**
- * Scanner의 느린 성능을 BufferedReader + StringTokenizer로 극복함  
- *
- */
-class Reader {  
-    static BufferedReader reader;
-    static StringTokenizer tokenizer;
-
-    /** call this method to initialize reader for InputStream */
-    static void init(InputStream input) {
-        reader = new BufferedReader(
-                     new InputStreamReader(input) );
-        tokenizer = new StringTokenizer("");
-    }
-
-    /** get next word */
-    static String next() throws IOException {
-        while ( ! tokenizer.hasMoreTokens() ) {
-            //TODO add check for eof if necessary
-            tokenizer = new StringTokenizer(
-                   reader.readLine() );
-        }
-        return tokenizer.nextToken();
-    }
-
-    static int nextInt() throws IOException {
-        return Integer.parseInt( next() );
-    }
 	
-    static double nextDouble() throws IOException {
-        return Double.parseDouble( next() );
-    }
+	/**
+	 * Scanner의 느린 성능을 BufferedReader + StringTokenizer로 극복함  
+	 *
+	 */
+	static class Reader {  
+	    static BufferedReader reader;
+	    static StringTokenizer tokenizer;
+
+	    static void init(InputStream input) {
+	        reader = new BufferedReader(
+	                     new InputStreamReader(input) );
+	        tokenizer = new StringTokenizer("");
+	    }
+
+	    static String next() throws IOException {
+	        while ( ! tokenizer.hasMoreTokens() ) {
+	            tokenizer = new StringTokenizer(
+	                   reader.readLine() );
+	        }
+	        return tokenizer.nextToken();
+	    }
+
+	    static int nextInt() throws IOException {
+	        return Integer.parseInt( next() );
+	    }
+		
+	    static double nextDouble() throws IOException {
+	        return Double.parseDouble( next() );
+	    }
+	}
+
 }
+
