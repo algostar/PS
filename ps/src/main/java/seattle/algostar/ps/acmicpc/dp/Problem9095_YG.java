6da018043ad59b1dd745163c481e875e042e1190
yonggyun.jung <yonggyun.jung@navercorp.com>
2015-11-23 16:40:53 +0900

    9095, 8394,1904

---
+++ ps/src/main/java/seattle/algostar/ps/acmicpc/dp/Problem9095_YG.java
@@ -0,0 +1,43 @@
+/*
+ * @(#)Problem9095.java $version 2015. 11. 21.
+ *
+ * Copyright 2014 NHN Corp. All rights Reserved. 
+ * NHN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+package com.naver.shopping.codingPractice.dp;
+
+import java.util.Scanner;
+
+public class Problem9095_YG {
+	public static void main(String[] args){
+		new Problem9095_YG().run();
+	}
+	
+	int[] dp;
+	int n;
+	public void run(){
+		Scanner sc = new Scanner(System.in);
+	
+		int cases = sc.nextInt();
+		
+		while(cases-- > 0){
+			n = sc.nextInt();
+			dp = new int[n + 1];
+			
+			if(n == 1){
+				System.out.println(1);
+			}else if(n == 2){
+				System.out.println(2);
+			}else{
+				dp[0] = 1; dp[1] = 1; dp[2] = 2;
+				
+				for(int i = 3; i <= n; i++){
+					dp[i] = dp[i -1] + dp[i-2] + dp[i-3];
+				}
+				
+				System.out.println(dp[n]);
+			}
+		}
+	}
+}
