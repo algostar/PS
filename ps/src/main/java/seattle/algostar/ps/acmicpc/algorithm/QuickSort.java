78f76681308300d9b615ae6d671f09ef1b1faeb8
jiwan, Jeong <mosaick2@naver.com>
2015-12-01 06:24:39 +0900

    quicksort

---
+++ ps/src/main/java/seattle/algostar/ps/acmicpc/algorithm/QuickSort.java
@@ -0,0 +1,43 @@
+package seattle.algostar.ps.acmicpc.algorithm;
+
+import java.util.Arrays;
+
+public class QuickSort {
+	public static void main(String[] args) {
+		new QuickSort().run();
+	}
+
+	private void run() {
+		int[] A = {3, 5, 1, 2, 4, 7, 6};
+		
+		quickSort(A, 0, A.length-1);
+		
+		System.out.println(Arrays.toString(A));
+	}
+
+	private void quickSort(int[] A, int left, int right) {
+		if (right > left) {
+			int pivot = partition(A, left, right);
+			quickSort(A, left, pivot - 1);
+			quickSort(A, pivot + 1, right);
+		}
+	}
+
+	private int partition(int[] A, int left, int right) {
+		int pivot = right;
+		int store = left;
+		for (int ix = left; ix < right; ix++) {
+			if (A[ix] <= A[pivot]) {
+				swap(A, ix, store++);
+			}
+		}
+		swap(A, store, pivot);
+		return store;
+	}
+
+	private void swap(int[] A, int pivot, int right) {
+		int temp = A[pivot];
+		A[pivot] = A[right];
+		A[right] = temp;
+	}
+}
