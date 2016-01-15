c0b3f0a2b774296621d2b5e522fcd6a85f051adb
yonggyun.jung <yonggyun.jung@navercorp.com>
2016-01-15 18:16:41 +0900

    6988번 commit

--- ps/src/main/java/seattle/algostar/ps/acmicpc/study/week1/Problem6988_YG.java
+++ ps/src/main/java/seattle/algostar/ps/acmicpc/study/week1/Problem6988_YG.java
@@ -7,17 +7,46 @@
 
 package seattle.algostar.ps.acmicpc.study.week1;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
-/**
- * @author 쇼핑관리개발팀 정용균
- */
 public class Problem6988_YG {
 	public static void main(String[] args){
 		new Problem6988_YG().run();
 	}
 	
+	int[][] dp;
+	int n;
+	int[] a;
+	Map<Integer,Integer> m;
 	public void run(){
 		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		a = new int[n];
+		dp = new int[n][n]; m = new HashMap<Integer, Integer>();
+		for(int i=0;i<n;i++){
+			a[i] = sc.nextInt();
+			m.put(a[i], i);
+		}
+		int max = -1;
+		for(int i=0;i<n-1;i++){
+			for(int j=i+1;j<n;j++){
+				if(m.containsKey(a[j]-a[i]+a[j])){
+					dp[i][j] = a[i] + a[j];
+				}
+			}
+		}
+		
+		for(int i=0;i<n-1;i++){
+			for(int j=i+1;j<n;j++){
+				if(m.containsKey(a[j]-a[i]+a[j])){
+					int v = m.get(a[j]-a[i]+a[j]);
+					dp[j][v] = Math.max(dp[j][v], dp[i][j] + a[v]);
+					max = Math.max(dp[j][v], max);
+				}
+			}
+		}
+		System.out.println(max);
 	}
 }
