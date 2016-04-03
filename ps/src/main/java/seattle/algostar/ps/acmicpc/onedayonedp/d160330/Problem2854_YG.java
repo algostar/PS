afb7974cbebfef4a43a34694692e34e76c274ff0
yonggyun.jung <yonggyun.jung@navercorp.com>
2016-04-03 18:10:27 +0900

    1일 1dp

--- ps/src/main/java/seattle/algostar/ps/acmicpc/onedayonedp/d160330/Problem2854_YG.java
+++ ps/src/main/java/seattle/algostar/ps/acmicpc/onedayonedp/d160330/Problem2854_YG.java
@@ -13,9 +13,6 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-/**
- * @author 쇼핑관리개발팀 정용균
- */
 public class Problem2854_YG {
 	public static void main(String[] args) throws NumberFormatException, IOException{
 		new Problem2854_YG().run();
@@ -24,34 +21,33 @@ public class Problem2854_YG {
 	int n;
 	int[] a;
 	int[] b;
-	long[] cache;
+	long[][] cache;
 	
 	public void run() throws NumberFormatException, IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		n = Integer.valueOf(br.readLine());
 		a = new int[n+1];
 		b=new int[n+1];
-		cache=new long[n+1];
+		cache=new long[n+1][2];
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		StringTokenizer stt = new StringTokenizer(br.readLine());
 		for(int i=1;i<=n;i++){
 			a[i]=Integer.valueOf(st.nextToken());
-			if(i!=n) b[i]=Integer.valueOf(stt.nextToken());
+			if(i!=1) b[i]=Integer.valueOf(stt.nextToken());
 		}
 	
-		cache[0]=1;
+		cache[0][0]=1;
 		for(int i=1;i<=n;i++){
-			cache[i]+=cache[i-1]*(a[i]+b[i]);
-			cache[i]%=1000000007;
-			if(i==1) continue;
-			cache[i]+=cache[i-2]*b[i-1]*(b[i-1]-1);
-			cache[i]%=1000000007;
-			cache[i]+=cache[i-2]*a[i-1]*b[i-1];
-			cache[i]%=1000000007;
+			cache[i][0]+=cache[i-1][0]*(a[i]+b[i])+cache[i-1][1]*(a[i]+b[i]-1);
+			cache[i][0]%=1000000007;
+			if(i==n) continue;
+			cache[i][1]+=cache[i-1][0]*b[i+1];
+			cache[i][1]%=1000000007;
+			cache[i][1]+=cache[i-1][1]*b[i+1];
+			cache[i][1]%=1000000007;
 		}
 		
-		//System.out.println(cache[n]);
-		System.out.println(Arrays.toString(cache));
+		System.out.println(cache[n][0]+cache[n][1]);
 	}
 }
