16bcb8adf6fa62ba34786d94be20bfef5aff3c29
mosaick <mosaick2@gmail.com>
2017-05-26 07:39:33 +0900

    test1

---
+++ ps/src/test/java/seattle/algostar/ps/acmicpc/dp/Test1.java
@@ -0,0 +1,15 @@
+package seattle.algostar.ps.acmicpc.dp;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String a = "2016-10-20T08:46:00.135Z";
+
+        String b = a.substring(0, 19);
+
+        System.out.println(b);
+    }
+}
